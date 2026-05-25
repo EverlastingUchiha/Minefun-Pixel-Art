9c8e5f2595a91507046e1eeddf7bae9d8e727c51
Everlasting <277428663+EverlastingUchiha@users.noreply.github.com>
2026-05-26 00:40:58 +0530

    Update pixel.user.js

--- pixel.user.js
+++ pixel.user.js
@@ -6,6 +6,7 @@
 // @author       Itz_Krishna AKA Everlasting
 // @match        https://minefun.io/*
 // @match        https://*.minefun.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=minefun.io
 // @grant        none
 // ==/UserScript==
 
